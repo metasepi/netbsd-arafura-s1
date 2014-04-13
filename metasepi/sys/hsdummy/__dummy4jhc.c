f14ff9b50fe67412d8b22d10a193117b042f8390
Kiwamu Okabe <kiwamu@debian.or.jp>
2014-04-14 00:56:51 +0900

    Add GC profiler

--- metasepi/sys/hsdummy/__dummy4jhc.c
+++ metasepi/sys/hsdummy/__dummy4jhc.c
@@ -1,7 +1,10 @@
 #include "jhc_rts_header.h"
+#include "rts/conc.h"
 #include <__dummy4jhc.h>
 #include <sys/syslog.h>
 #include <sys/kprintf.h>
+#include <ddb/ddb.h>
+#include <machine/cpufunc.h>
 #include <sys/malloc.h>
 #undef malloc
 #undef free
@@ -125,3 +128,62 @@ uname(struct utsname *name)
 {
 	return -1;
 }
+
+#if _JHC_PROFILE || _JHC_PROFILE_GCONLY
+struct profile_stack {
+	u_int64_t count;
+	u_int64_t t_total;
+	u_int64_t t_max;
+	u_int64_t t_start;
+};
+
+/*
+ * xxx If there are many contexts, use this share and conflict.
+ * xxx The implementation has only one context.
+ */
+struct profile_stack gc_alloc_time;
+struct profile_stack gc_gc_time;
+
+void
+jhc_profile_push(struct profile_stack *ps)
+{
+        jhc_rts_lock();
+	ps->count++;
+	__asm __volatile("rdtsc":"=A"(ps->t_start)); // x86 only
+        jhc_rts_unlock();
+}
+
+void
+jhc_profile_pop(struct profile_stack *ps)
+{
+	u_int64_t t_now;
+        jhc_rts_lock();
+	__asm __volatile("rdtsc":"=A"(t_now)); // x86 only
+	t_now -= ps->t_start;
+	ps->t_total += t_now;
+	if (t_now > ps->t_max) {
+		ps->t_max = t_now;
+	}
+        jhc_rts_unlock();
+}
+
+void jhc_profile_reset(void);
+void
+jhc_profile_reset(void)
+{
+	memset(&gc_alloc_time, 0, sizeof(struct profile_stack));
+	memset(&gc_gc_time, 0, sizeof(struct profile_stack));
+}
+
+void jhc_profile_show(void);
+void
+jhc_profile_show(void)
+{
+	db_printf("_JHC_JGC_NAIVEGC=%d _JHC_JGC_MEGABLOCK_SHIFT=%d _JHC_JGC_BLOCK_SHIFT=%d\n",
+	    _JHC_JGC_NAIVEGC, _JHC_JGC_MEGABLOCK_SHIFT, _JHC_JGC_BLOCK_SHIFT);
+	db_printf("gc_alloc_time = {count=%llx t_total=%llx t_max=%llx}\n",
+	    gc_alloc_time.count, gc_alloc_time.t_total, gc_alloc_time.t_max);
+	db_printf("gc_gc_time = {count=%llx t_total=%llx t_max=%llx}\n",
+	    gc_gc_time.count, gc_gc_time.t_total, gc_gc_time.t_max);
+}
+#endif
